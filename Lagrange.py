e26fe4a11b5089fd60ca11808ace3ccbb814beae
George <george@MBP-George.mshome.net>
2021-12-03 19:21:18 +0700

    перепутал названия формул. сделан Лагранж, а не Ньютон

---
+++ Lagrange.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def Lagrange(x,Xn,Fxn):
+    n = len(Xn)
+    res = []
+    buff = 1.0
+    for i in range(n):
+        for j in range(n):
+            if(j==i):
+                continue
+            buff *= (x - Xn[j]) / (Xn[i] - Xn[j])
+        res.append(buff * Fxn[i])
+        buff = 1.0
+    return sum(res)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import buildPlot as plot
+import Lagrange as L
 import Newton as N
 
 Xn = np.array([1, 2, 3, 4, 5])
 Fxn = np.array([4., 2., 8., 1., -1.])
 
 plot.build(Xn,Fxn)
-plot.buildInterpolated(N.Newton,Xn,Fxn)
+plot.buildInterpolated(L.Lagrange,Xn,Fxn)
 
 
